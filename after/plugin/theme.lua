7c2f831c62e3400b4b134403c0219f36e7d900c4
sekaiking <afaithraf@gmail.com>
2023-12-02 09:00:22 +0100

    update lualine config

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -5,7 +5,12 @@ require("solarized-osaka").setup({
         sidebars = "transparent",
         comments = { italic = false },
         keywords = { italic = false },
-    }
+    },
+    on_highlights = function(hl, c)
+        hl.TroubleNormal = {
+            bg = c.bg_dark,
+        }
+    end,
 });
 
 vim.cmd [[colorscheme solarized-osaka]]
@@ -13,9 +18,41 @@ vim.cmd [[colorscheme solarized-osaka]]
 -- Nvim Statusline
 require('lualine').setup {
     options = {
-        -- ... your lualine config
-        theme = 'solarized-osaka'
-        -- ... your lualine config
+        icons_enabled = true,
+        theme = 'nightfly'
+    },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = {
+            'diagnostics',
+            {
+                'filename',
+                file_status = true,
+                path = 1,
+            },
+            'branch',
+            'diff',
+        },
+        lualine_c = {
+            {
+                'searchcount',
+                maxcount = 999,
+                timeout = 500,
+            }
+        },
+        lualine_x = {
+            {
+                'datetime',
+                style = '%H:%M:%S',
+            },
+            'encoding',
+            {
+                'filetype',
+                colored = true,
+            },
+        },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
     }
 }
 
